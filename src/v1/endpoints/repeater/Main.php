5d877d416546f2595ab6248a463041def844099e
HB9AKM <info@hb9akm.ch>
2021-08-23 18:41:21 +0200

    Code style improvement

--- src/v1/endpoints/repeater/Main.php
+++ src/v1/endpoints/repeater/Main.php
@@ -7,11 +7,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class Main extends \HB9AKM\API\Endpoint {
     protected function init(): void {
-        $this->main->getApp()->get('/v1/repeater', function (Request $request, Response $response, $args) {
-            $response->getBody()->write(
-                file_get_contents(__DIR__ . '/voiceRepeater.json')
-            );
-            return $response->withHeader('Content-Type', 'application/json');
-        });
+        $this->main->getApp()->get(
+            '/v1/repeater',
+            function (Request $request, Response $response, $args) {
+                $response->getBody()->write(
+                    file_get_contents(__DIR__ . '/voiceRepeater.json')
+                );
+                return $response->withHeader('Content-Type', 'application/json');
+            }
+        );
     }
 }
